72541e9ab42acb7de5ef9f58c1566aa501653b2f
taianaazevedo <taianazev@gmail.com>
2023-01-08 22:38:17 -0300

    rota sign-up ok

--- src/app.js
+++ src/app.js
@@ -3,8 +3,49 @@ import cors from "cors"
 
 const app = express()
 
+app.use(express.json())
+
 app.use(cors())
 
-app.listen(5000, () => {
-    console.log("Ta funcionando")
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`)
 })
+
+let usuariosLogados = [
+    // {
+    // username: "dwight schrute",
+    // avatar: "https://www.looper.com/img/gallery/the-best-time-dwight-schrute-ever-broke-character-on-the-office/intro-1620063081.jpg"
+    // }
+]
+let tweets = []
+
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar} = req.body
+    const usuario = {
+        username: username,
+        avatar: avatar
+    }
+    usuariosLogados.push(usuario);
+	res.status(201).send("OK");
+})
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body
+    const tweetUsuario = {
+        username: username,
+        tweet: tweet
+    }
+    if(!usuariosLogados.find((u) => u.username === username)){
+        return res.send("UNAUTHORIZED")
+    } else {
+        tweets.push(tweetUsuario)
+        res.send("OK")
+    }
+
+
+})
+
+
+
